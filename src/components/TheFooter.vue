a13477ec47dcfa0596e24a34217864808d58dbee
Vicente <vmedina@omitsis.com>
2022-11-04 16:13:01 +0100

    :sparkles: added feature for home, and links to on footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -24,6 +24,40 @@
         >
           Copyright &copy; 2022. All rights reserved.
         </p>
+
+        <p
+          class="
+            mt-4
+            text-center text-sm text-gray-500
+            dark:text-gray-400
+            lg:mt-0 lg:text-right
+          "
+        >
+          Made with
+          <span class="text-red-500">&hearts;</span> by
+          <a href="https://medinarodriguez.com/" target="_blank">Vicente</a>
+        </p>
+        <a
+          href="https://github.com/vmedina-rod/todo-app"
+          target="_blank"
+          class="
+            text-teal-600
+            dark:text-teal-300
+            hover:text-teal-500
+            dark:hover:text-teal-400
+            ml-1
+          "
+        >
+          <img
+            src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg"
+            width="20"
+            height="20"
+            class="d-block"
+            loading="lazy"
+            decoding="async"
+            alt="GitHub mark"
+          />
+        </a>
       </div>
     </div>
   </footer>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -453,5 +453,261 @@
         >
       </div>
     </section>
+
+    <section
+      class="
+        py-8
+        px-4
+        md:px-8
+        lg:py-16
+        2xl:py-40 2xl:px-60
+        mx-auto
+        grid grid-cols-1
+        gap-8
+        lg:grid-cols-12
+        relative
+      "
+    >
+      <div class="col-span-6 relative">
+        <img
+          class="hidden xl:block absolute z-0 right-[9px] top-[13px]"
+          src="@/assets/app_bg.svg"
+          alt=""
+        />
+        <div class="relative aspect-[366/729] mx-auto max-w-[320px]">
+          <div
+            class="
+              shadow-xl
+              absolute
+              inset-y-[calc(1/729*100%)]
+              right-[calc(5/729*100%)]
+              left-[calc(7/729*100%)]
+              rounded-[calc(58/366*100%)/calc(58/729*100%)]
+            "
+          ></div>
+          <div
+            class="
+              absolute
+              top-[calc(23/729*100%)]
+              left-[calc(23/366*100%)]
+              grid
+              h-[calc(686/729*100%)]
+              w-[calc(318/366*100%)]
+              transform
+              grid-cols-1
+              overflow-hidden
+              bg-gray-900
+              pt-[calc(23/318*100%)]
+            "
+          >
+            <div class="flex flex-col">
+              <!-- content here -->
+              <div class="bg-slate-900 w-full h-full">
+                <div class="flex items-center">
+                  <img
+                    class="w-8 mt-5 mx-auto text-white"
+                    src="@/assets/menu.svg"
+                    alt="logo"
+                  />
+                  <img
+                    class="w-8 mt-5 mx-auto"
+                    src="@/assets/todovue-icon.webp"
+                    alt="logo"
+                  />
+                  <img
+                    class="w-8 mt-5 mx-auto text-white"
+                    src="@/assets/logout.svg"
+                    alt="logo"
+                  />
+                </div>
+
+                <div class="rounded-2xl bg-slate-50 mt-4 h-full p-4">
+                  <div
+                    class="
+                      flex
+                      justify-between
+                      p-1
+                      mb-4
+                      border-b border-slate-300
+                      px-2
+                      pt-4
+                      pb-4
+                    "
+                  >
+                    <p class="text-sm font-medium">My Tasks</p>
+                    <img class="w-5 h-5" src="@/assets/close.svg" alt="logo" />
+                  </div>
+
+                  <div
+                    class="
+                      border
+                      bg-slate-100
+                      rounded-md
+                      p-2
+                      max-w-sm
+                      w-full
+                      mx-auto
+                      mb-4
+                    "
+                  >
+                    <div class="flex items-center space-x-4">
+                      <div class="w-min">
+                        <div
+                          class="
+                            h-4
+                            w-4
+                            bg-transparent
+                            rounded-full
+                            border border-slate-500
+                          "
+                        ></div>
+                      </div>
+                      <div class="flex-1 py-1 col-span-11">
+                        <p class="text-md font-medium">Go to gym 🏋🏻‍♂️</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div
+                    class="
+                      border
+                      bg-slate-100
+                      rounded-md
+                      p-2
+                      max-w-sm
+                      w-full
+                      mx-auto
+                      mb-4
+                    "
+                  >
+                    <div class="flex items-center space-x-4">
+                      <div class="w-min">
+                        <div
+                          class="
+                            h-4
+                            w-4
+                            bg-transparent
+                            rounded-full
+                            border border-slate-500
+                          "
+                        ></div>
+                      </div>
+                      <div class="flex-1 py-1 col-span-11">
+                        <p class="text-md font-medium">Ironhack Project 🚀</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div
+                    class="
+                      border
+                      bg-slate-100
+                      rounded-md
+                      p-2
+                      max-w-sm
+                      w-full
+                      mx-auto
+                      mb-4
+                    "
+                  >
+                    <div class="flex items-center space-x-4">
+                      <div class="w-min">
+                        <div
+                          class="
+                            h-4
+                            w-4
+                            bg-transparent
+                            rounded-full
+                            border border-slate-500
+                          "
+                        ></div>
+                      </div>
+                      <div class="flex-1 py-1 col-span-11">
+                        <p class="text-md font-medium">Code landing page 👨‍💻</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div
+                    class="
+                      border
+                      bg-slate-100
+                      rounded-md
+                      p-2
+                      max-w-sm
+                      w-full
+                      mx-auto
+                      mb-4
+                    "
+                  >
+                    <div class="flex items-center space-x-4">
+                      <div class="w-min">
+                        <div
+                          class="
+                            h-4
+                            w-4
+                            bg-transparent
+                            rounded-full
+                            border border-slate-500
+                          "
+                        ></div>
+                      </div>
+                      <div class="flex-1 py-1 col-span-11">
+                        <p class="text-md font-medium">Take a beer 🍻</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <svg
+            viewBox="0 0 366 729"
+            aria-hidden="true"
+            class="
+              pointer-events-none
+              absolute
+              inset-0
+              h-full
+              w-full
+              fill-gray-500
+            "
+          >
+            <path
+              fill="#F2F2F2"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M300.092 1c41.22 0 63.223 21.99 63.223 63.213V184.94c-.173.184-.329.476-.458.851.188-.282.404-.547.647-.791.844-.073 2.496.257 2.496 2.157V268.719c-.406 2.023-2.605 2.023-2.605 2.023a7.119 7.119 0 0 1-.08-.102v394.462c0 41.213-22.001 63.212-63.223 63.212h-95.074c-.881-.468-2.474-.795-4.323-.838l-33.704-.005-.049.001h-.231l-.141-.001c-2.028 0-3.798.339-4.745.843H66.751c-41.223 0-63.223-21.995-63.223-63.208V287.739c-.402-.024-2.165-.23-2.524-2.02v-.973A2.039 2.039 0 0 1 1 284.62v-47.611c0-.042.001-.084.004-.126v-.726c0-1.9 1.652-2.23 2.496-2.157l.028.028v-16.289c-.402-.024-2.165-.23-2.524-2.02v-.973A2.039 2.039 0 0 1 1 214.62v-47.611c0-.042.001-.084.004-.126v-.726c0-1.9 1.652-2.23 2.496-2.157l.028.028v-26.041a2.26 2.26 0 0 0 .093-.236l-.064-.01a3.337 3.337 0 0 1-.72-.12l-.166-.028A2 2 0 0 1 1 135.62v-24.611a2 2 0 0 1 1.671-1.973l.857-.143v-44.68C3.528 22.99 25.53 1 66.75 1h233.341ZM3.952 234.516a5.481 5.481 0 0 0-.229-.278c.082.071.159.163.228.278Zm89.99-206.304A4.213 4.213 0 0 0 89.727 24H56.864C38.714 24 24 38.708 24 56.852v618.296C24 693.292 38.714 708 56.864 708h250.272c18.15 0 32.864-14.708 32.864-32.852V56.852C340 38.708 325.286 24 307.136 24h-32.864a4.212 4.212 0 0 0-4.213 4.212v2.527c0 10.235-8.3 18.532-18.539 18.532H112.48c-10.239 0-18.539-8.297-18.539-18.532v-2.527Z"
+            ></path>
+            <rect
+              x="154"
+              y="29"
+              width="56"
+              height="5"
+              rx="2.5"
+              fill="#D4D4D4"
+            ></rect></svg
+          ><img
+            alt=""
+            src="@/assets/phone-frame.svg"
+            width="366"
+            height="729"
+            decoding="async"
+            class="pointer-events-none absolute inset-0 h-full w-full"
+          />
+        </div>
+      </div>
+      <div class="col-span-6 xl:place-self-center mb-8 mt-8 xl:mt-0 lg:mb-0">
+        <h2 class="text-3xl xl:text-4xl font-medium text-center lg:text-left">
+          Add your tasks.<br />
+          Organize your life.<br />
+          Achieve more every day.
+        </h2>
+        <p class="my-8 max-w-lg mx-auto lg:mx-0 text-center lg:text-left">
+          Add tasks like “Go to gym” to fill your to-do list in seconds using
+          TodoVue’s.
+        </p>
+      </div>
+    </section>
   </main>
 </template>
